a1acebfa0630c1022258ab654b032e8eb2226329
Джан Али <D.Ali21g@pmgrz.net>
2026-04-02 12:43:21 +0300

    Extend user identity fields

---
+++ AutoRent.Infrastructure/Migrations/20260401142213_AddPersonalIdentityFieldsToApplicationUser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoRent.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPersonalIdentityFieldsToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DriverLicenseIssueDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "EGN",
+                table: "AspNetUsers",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityCardNumber",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DriverLicenseIssueDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "EGN",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityCardNumber",
+                table: "AspNetUsers");
+        }
+    }
+}
